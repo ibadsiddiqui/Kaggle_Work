e56b6d9913944b3356662c1f43ac78a22973c5f9
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-11-18 16:43:02 +0500

    Data prediction added for houses data

--- Course/Machine_Learning/01-Analyzing Home Data/Exercise_First_Machine_Learning_Model.py
+++ Course/Machine_Learning/01-Analyzing Home Data/Exercise_First_Machine_Learning_Model.py
@@ -1,10 +1,18 @@
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+
 
 melbourn_file_path = "./train.csv"
 
 melbourn_data = pd.read_csv(melbourn_file_path)
-#print(melbourn_data)
 
 y = melbourn_data.SalePrice
+
 features = ["LotArea","YearBuilt" ,"1stFlrSF" ,"2ndFlrSF" , "FullBath","BedroomAbvGr" ,"TotRmsAbvGrd"]
 X = melbourn_data[features]
+
+melbourn_data_model = DecisionTreeRegressor(random_state=1)
+melbourn_data_model.fit(X,y)
+
+predictions = melbourn_data_model.predict(X)
+print(predictions)
